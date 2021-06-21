25274fd80d16c01ac366300bb43ca62b2d6cb3b2
Revaz Surguladze <revazsurg@gmail.com>
2021-06-21 18:58:46 +0400

    Update refresher_pw.js

--- refresher_pw.js
+++ refresher_pw.js
@@ -1,7 +1,7 @@
-const { chromium } require('playwright');
+const chromium require('playwright');
 
 (async () => {
-  const browser = await webkit.launch();
+  const browser = await chromium .launch();
   const page = await browser.newPage();
   await page.goto('http://www.google.com/');
   await page.screenshot({ path: `google.png` });
