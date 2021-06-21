4f1369c2100a115af0bb697600beba53e8fa3985
Revaz Surguladze <revazsurg@gmail.com>
2021-06-21 21:11:30 +0400

    Update refresher_selenium.js

--- refresher_selenium.js
+++ refresher_selenium.js
@@ -2,10 +2,11 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 
 (async function example() {
   let driver = await new Builder().forBrowser('chrome').build();
+console.log('1');
     await driver.get('https://www.google.com/');
-
+console.log('2');
     console.log('page visited');    
-
+console.log('3');
     await driver.quit();
-
+console.log('4');
 })();
