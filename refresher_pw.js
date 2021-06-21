235974a9ad9e04a1638f33c7c84fa04cefbd6dc2
Revaz Surguladze <revazsurg@gmail.com>
2021-06-21 19:33:08 +0400

    Update refresher_pw.js

--- refresher_pw.js
+++ refresher_pw.js
@@ -1,11 +1,15 @@
-console.log('start script');
-const { chromium } = require('playwright');
-
+const { webkit } = require('playwright');
+console.log('prefunction);
 (async () => {
-  const browser = await chromium.launch();
+  console.log('start');
+  const browser = await webkit.launch();
+  console.log('browser launched');
   const page = await browser.newPage();
-  await page.goto('https://www.google.com/');
-  await page.screenshot({ path: `google.png` });
-  console.log('screenshot captured');
+  console.log('page created');
+  await page.goto('http://whatsmyuseragent.org/');
+  console.log('page visited');
+  await page.screenshot({ path: `example.png` });
+  console.log('screenshot taken');
   await browser.close();
+  console.log('browser closed');
 })();
